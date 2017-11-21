50f2745b7481e71882b8f0104805ea97b3b6c774
vahlo <kallbom.viktor@gmail.com>
2017-11-21 16:21:41 +0100

    Files
    uppladdning av alla filer, Viktors 0,0

---
+++ Digcash 0.0/wpfloginscreen/LoginScreen.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfloginscreen
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            //Kopplingen till DB
+            SqlConnection sqlCon = new SqlConnection("Data Source=VIKTORKLLBOAA42\\SQLEXPRESS;Initial Catalog=LogonDB;Integrated Security=True");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tbluser WHERE UserID=@UserID AND Password=@Password";
+
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+
+                sqlCmd.Parameters.AddWithValue("@UserID",txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                string UserCheck = txtUsername.Text;
+                
+
+                if (count == 1)
+                {
+
+                    string admin = "5";
+                    string sales = "3";
+                    string casher = "2";
+
+                    if (UserCheck.Substring(0,1) == admin)
+                    
+                    {
+                        MessageBox.Show("Welcome Admin with UserID nr : " + UserCheck);
+                        //Skickar Admin till signup window
+                        signup newUser = new signup();
+                        newUser.Show();
+                        this.Close();
+                    }
+
+                    else if (UserCheck.Substring(0, 1) == casher)
+
+                    {
+                        MessageBox.Show("Welcome casher, with userID : " + UserCheck);
+                        //Skickar till Kassa window
+                        Window1 cash = new Window1();
+                        cash.Show();
+                        this.Close();
+
+                    }
+
+                    else if (UserCheck.Substring(0, 1) == sales)
+
+                    {
+                        MessageBox.Show("Welcome Sales, with userID : " + UserCheck);
+                        //Skickar till main window
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+
+                    }
+
+
+
+                }
+
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
---
+++ Digcash 0.0/wpfloginscreen/signup.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace wpfloginscreen
+{
+    /// <summary>
+    /// Interaction logic for signup.xaml
+    /// </summary>
+    public partial class signup : Window
+    {
+        public signup()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            //Kopplingen till Viktors SQL Server
+            SqlConnection sqlCon = new SqlConnection("Data Source=VIKTORKLLBOAA42\\SQLEXPRESS;Initial Catalog=LogonDB;Integrated Security=True");
+            {
+                sqlCon.Open();
+
+                //"Useradd" är query för att lägga till ny person i  vårt table
+                SqlCommand sqlCmd = new SqlCommand("UserAdd", sqlCon);
+                
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                
+
+                //Lägger in värderna från användaren till rätt positioner i tabelen
+
+                sqlCmd.Parameters.AddWithValue("@UserID", txtUserID.Text.Trim()); //Trim tar bort mellanslag i början evt slutet
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password.Trim());
+                sqlCmd.ExecuteNonQuery(); //exicute ovanstående
+                MessageBox.Show("Registration is Sucessfull");
+                Clear();
+
+            }
+
+            void Clear()
+            {
+                txtUsername.Text = txtUserID.Text = txtPassword.Password = "";
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginScreen logOut = new LoginScreen();
+            logOut.Show();
+            this.Close();
+        }
+    }
+}
